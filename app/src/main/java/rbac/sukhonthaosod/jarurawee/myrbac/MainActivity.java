1956634b43ae9008b5977359f57bc6367dc026f4
jarurawee <eye02899@gmail.com>
2017-08-05 14:48:10 +0700

    Before Lunch1

--- app/src/main/java/rbac/sukhonthaosod/jarurawee/myrbac/MainActivity.java
+++ app/src/main/java/rbac/sukhonthaosod/jarurawee/myrbac/MainActivity.java
@@ -3,11 +3,23 @@ package rbac.sukhonthaosod.jarurawee.myrbac;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rbac.sukhonthaosod.jarurawee.myrbac.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //All Fragment
+        if (savedInstanceState == null){
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.mainContainer, mainFragment)
+                    .commit();
+        }
+    }   //Main Method
+}   //Main Class
