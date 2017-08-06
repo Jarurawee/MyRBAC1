4ac44ed57f0966086c66439b6cbf3e6f7104787f
Administrator <eye02899@gmail.com>
2017-08-06 10:38:30 +0700

    before1

---
+++ app/src/main/java/rbac/sukhonthaosod/jarurawee/myrbac/fragment/SignUpFragment.java
@@ -0,0 +1,20 @@
+package rbac.sukhonthaosod.jarurawee.myrbac.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 6/8/2560.
+ */
+public class SignUpFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}// Main Class
