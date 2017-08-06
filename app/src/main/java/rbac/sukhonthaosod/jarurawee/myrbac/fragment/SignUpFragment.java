3a272a9dc6f09e8e15f9ef32b00886c982dd6ddc
Administrator <eye02899@gmail.com>
2017-08-06 12:51:43 +0700

    before

--- app/src/main/java/rbac/sukhonthaosod/jarurawee/myrbac/fragment/SignUpFragment.java
+++ app/src/main/java/rbac/sukhonthaosod/jarurawee/myrbac/fragment/SignUpFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import rbac.sukhonthaosod.jarurawee.myrbac.R;
@@ -15,6 +16,7 @@ import rbac.sukhonthaosod.jarurawee.myrbac.R;
  */
 public class SignUpFragment extends Fragment{
 
+    private String nameString, userString, passwordString;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -28,8 +30,51 @@ public class SignUpFragment extends Fragment{
         super.onActivityCreated(savedInstanceState);
 
         //Back Controller
+        backController();
+
+        //Save Controller
+        saveController();
+
+
+    }
+
+    private void saveController() {
+        ImageView imageView = (ImageView) getView().findViewById(R.id.imvSave);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value From EditText
+                EditText nameEditText = (EditText) getView().findViewById(R.id.edtName);
+                EditText userEditText = (EditText) getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = (EditText) getView().findViewById(R.id.edtPassword);
+
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+                    //Have Space
+
+                } else {
+                    //No Space
+
+                }
+
+
+
+            } //onClick
+        });
+    }
+
+    private void backController() {
         ImageView imageView = (ImageView) getView().findViewById(R.id.imvBack);
-
-
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
     }
 }// Main Class
